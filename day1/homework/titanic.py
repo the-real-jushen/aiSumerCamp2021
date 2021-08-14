5f008de3f3fa29b91e05841dd5b08fdf9a297ba6
Fengming Xue <1349976364@qq.com>
2021-08-14 15:49:36 +0800

    content day 1

---
+++ day1/homework/titanic.py
@@ -0,0 +1,11 @@
+# %% [markdown]
+
+"""
+Homework:
+
+The folder '~//data//homework' contains data of Titanic with various features and survivals.
+
+Try to use what you have learnt today to predict whether the passenger shall survive or not.
+
+Evaluate your model with 'test.csv'.
+"""
